8174c984b7b0d334cd0e7e98596ef1075d6a8a66
Nicolás <78928407+Nikolaii-L@users.noreply.github.com>
2023-07-20 22:43:18 -0700

    navbar-wip

---
+++ src/components/UI/Navbar.jsx
@@ -0,0 +1,10 @@
+import '../Styles/Navbar.css';
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      Navbar
+      <div className="navbar_item">Item Navbar</div>
+    </div>
+  );
+};
